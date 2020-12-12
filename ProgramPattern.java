ff51b10da50e54d9a3e6ba8e31418eab2d599e01
suriyan05c <suriyan05c@gmail.com>
2020-12-12 07:36:04 -0800

    Edited

---
+++ ProgramPattern.java
@@ -0,0 +1,54 @@
+package project_pattern;
+
+public class ProgramPattern 
+{
+
+public static void main(String[] args)
+{
+for(int colSp=1;colSp<=4;colSp++)
+{
+System.out.print(" ");
+}
+for(int colNo=1;colNo<=1;colNo++)
+{
+System.out.print(colNo);
+}
+System.out.println();
+for(int colSp=1;colSp<=3;colSp++)
+{
+System.out.print(" ");
+}
+for(int colNo=2;colNo<=3;colNo++)
+{
+System.out.print(colNo);
+}
+System.out.println();
+for(int colSp=1;colSp<=2;colSp++)
+{
+System.out.print(" ");
+}
+for(int colNo=4;colNo<=6;colNo++)
+{
+System.out.print(colNo);
+}
+System.out.println();
+for(int colSp=1;colSp<=1;colSp++)
+{
+System.out.print(" ");
+}
+for(int colNo=7;colNo<=10;colNo++)
+{
+System.out.print(colNo);
+}
+System.out.println();
+for(int colSp=1;colSp<=0;colSp++)
+{
+System.out.print(" ");
+}
+for(int colNo=11;colNo<=15;colNo++)
+{
+System.out.print(colNo);
+}
+System.out.println();
+}
+}
